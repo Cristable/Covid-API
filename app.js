3c3148aaa87d8bcf1a6d4e13bb6892f91ba73471
Cristable <cristableh7@gmail.com>
2020-08-23 23:36:44 +0000

    Update

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
-const express = require("express");
+const express = require('express');
 
-const countries = require("./lib/countries");
-const getCases = require("./lib/cases");
-
-const url =
-  "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_daily_reports/08-19-2020.csv";
+const Case = require('./lib/database');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
 
-app.get("/", function (req, res) {
-  res.status(200).json(getCases(url, countries));
+app.get('/api/v1', function (req, res) {
+	let country = req.query.country;
+	let date = req.query.date;
+
+	if (country == undefined) {
+		Case.find((err, docs) => {
+			if (err) {
+				res.status(400).send();
+			}
+			res.status(200).json(docs);
+		});
+	}
+
+	const firstChar = country[0].toUpperCase();
+	country = firstChar + country.slice(1);
+
+	Case.find({ Country: country }, (err, docs) => {
+		if (err) {
+			res.status(400).send();
+			return;
+		}
+		res.status(200).json(docs);
+		return;
+	});
 });
 
 app.listen(PORT, function () {
-  console.log(`server is running on port ${PORT} .....`);
+	console.log(`server is running on port ${PORT} .....`);
 });
